2d7d207130af309d6135fa772b75c5497df8f173
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-09-23 18:51:56 -0700

    animation toggle attempts not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 var api_key = "R9dyDzD5AFmrzh6cOKVjf7TBTQOh9rzg";
 var queryURL = [];
 var clickedMood;
+var state="still";
 
 // Premade mood buttons in array
 var moods = ["happy","sad","mad","loving"];
@@ -13,9 +14,9 @@ for (j=0;j<(moods.length);j++){
     $("#btn-"+j).text(moods[j]).attr("value",j);
     //create search url for each button
     queryURL[j]="https://api.giphy.com/v1/gifs/search?q="+moods[j]+"&api_key="+api_key+"&limit=10";
-}
+};
 
-
+// make buttons clickable
 $(".clickable").click(function(){
     clickedMood = $(this).attr("value");
 
@@ -24,12 +25,12 @@ $(".clickable").click(function(){
         url: queryURL[parseInt(clickedMood)],
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        //console.log(response);
         for (i=0;i<10;i++){
             var giphURLa = response.data[i].images.downsized_medium.url;
             //$("#endP").prepend("<img src="+giphURLa+" width=200px height=200px>");
             var giphURLs = response.data[i].images.downsized_still.url;
-            $("#endP").prepend("<img src="+giphURLs+" data-animate="+giphURLa+" data-still="+giphURLs+" data-state=still>");
+            $("#endP").prepend("<img class=pic src="+giphURLs+" data-animate="+giphURLa+" data-still="+giphURLs+" data-state=still>");
             var giphRating = response.data[i].rating;
             var giphTitle = response.data[i].title;
             $("#endP").prepend($("<h5>").text("Rating: "+giphRating+" Title: "+giphTitle));
@@ -37,14 +38,16 @@ $(".clickable").click(function(){
     });
 });
 
-$("<img>").click(function(){
-    var state=$(this).attr("data-state");
-    console.log(state);
-    if(state==="still"){
+// toggle animation on click
+$(".pic").on("click",function(){
+    state=$(this).attr("data-state");
+    if(state=="still"){
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
-      } else {
+    } else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
-})
+});
+
+//
