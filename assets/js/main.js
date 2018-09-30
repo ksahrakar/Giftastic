d373b68df863e1c4dae41f0d96f29c72a812dc50
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-09-29 23:41:19 -0700

    final fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ var api_key = "R9dyDzD5AFmrzh6cOKVjf7TBTQOh9rzg";
 var queryURL = [];
 var clickedMood;
 var state="still";
+var numOfGifs = 10;
 
 // Premade mood buttons in array
 var moods = ["happy","sad","mad","loving"];
@@ -10,16 +11,16 @@ var moods = ["happy","sad","mad","loving"];
 for (j=0;j<(moods.length);j++){
     $("#btn-place").prepend($("<button>").attr("id",("btn-"+j)));
     //label each button
-    $("#btn-"+j).text(moods[j]).attr("class","clickable");
+    $("#btn-"+j).text(moods[j]).attr("class","clickable btn btn-secondary");
     //add a value to each button corresponding to its location in array
     $("#btn-"+j).text(moods[j]).attr("value",j);
     //create search url for each button
-    queryURL[j]="https://api.giphy.com/v1/gifs/search?q="+moods[j]+"&api_key="+api_key+"&limit=10";
+    queryURL[j]="https://api.giphy.com/v1/gifs/search?q="+moods[j]+"&api_key="+api_key+"&limit="+numOfGifs;
 };
 
 // make buttons clickable
 $(document.body).on("click",".clickable",function(){
-//$(".clickable").on("click",function(){
+
     clickedMood = $(this).attr("value");
 
     // Make query through API and prepend gif's and ratings
@@ -27,21 +28,20 @@ $(document.body).on("click",".clickable",function(){
         url: queryURL[parseInt(clickedMood)],
         method: "GET"
     }).then(function(response){
-        console.log(response);
-        for (i=0;i<10;i++){
+        //console.log(response);
+        for (i=0;i<numOfGifs;i++){
             var gifClabel = "["+clickedMood+"]["+i+"]";
 
             var giphURLa = response.data[i].images.fixed_width.url;
-            //$("#gifs").prepend("<img src="+giphURLa+" width=200px height=200px>");
+            
             var giphURLs = response.data[i].images.fixed_width_still.url;
             var giphRating = response.data[i].rating;
             var giphTitle = response.data[i].title;
 
             var newElement = $("<span>").attr({class:"gif",id:gifClabel})
-            // newElement.css("display", "flex");
-
+            
             newElement.prepend("<img class='pic' src="+giphURLs+" data-animate="+giphURLa+" data-still="+giphURLs+" data-state="+state+">");
-            //var gifLocation = "g"+gifClabel;
+            
             newElement.prepend($("<h6>").text(giphTitle));
             newElement.prepend($("<h6>").text("Rating: "+giphRating));
 
@@ -67,12 +67,14 @@ $(document.body).on("click",".pic",function(){
 // add a new mood
 $("#newMoodBtn").on("click", function() {
     //get value of input field and add to array
+    // numOfGifs = $("#resultNum").attr("value");
+    // console.log(numOfGifs);
     y=moods.length;
     var x = $("#newMood").val();
     moods.push(x);
     $("#btn-place").prepend($("<button>").attr("id",("btn-"+y)));
-    $("#btn-"+y).text(moods[y]).attr("class","clickable");
+    $("#btn-"+y).text(moods[y]).attr("class","clickable btn btn-secondary");
     $("#btn-"+y).text(moods[y]).attr("value",y);
-    queryURL[y]="https://api.giphy.com/v1/gifs/search?q="+moods[y]+"&api_key="+api_key+"&limit=10";
-    $("#newMood").attr("value","");
+    queryURL[y]="https://api.giphy.com/v1/gifs/search?q="+moods[y]+"&api_key="+api_key+"&limit="+numOfGifs;
+
 })
